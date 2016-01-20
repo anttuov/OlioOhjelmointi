193f21ab5880a320b6caab803a8b0a49e3ab4736
anttuov <k2470@student.jamk.fi>
2016-01-20 19:38:36 +0200

    a

---
+++ D3T5/D3T5.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3T5
+{
+    class Opiskelija
+    {
+        public string Nimi;
+        public int OpNum;
+        public string Osoite;
+
+        public void PrintData()
+        {
+            Console.WriteLine("Nimi: " + Nimi);
+            Console.WriteLine("Opiskelijanumero: " + OpNum);
+            Console.WriteLine("Osoite: " + Osoite);
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija[] opiskelijat = new Opiskelija[5];
+            opiskelijat[0] = new Opiskelija { Nimi = "Matti", OpNum = 1234, Osoite = "Katu 1" };
+            opiskelijat[1] = new Opiskelija { Nimi = "Antti", OpNum = 2222, Osoite = "Katu 31" };
+            opiskelijat[2] = new Opiskelija { Nimi = "Aku", OpNum = 7777, Osoite = "Kuja 5467" };
+            opiskelijat[3] = new Opiskelija { Nimi = "Saten", OpNum = 666, Osoite = "Tie 666" };
+            opiskelijat[4] = new Opiskelija { Nimi = "asd", OpNum = 1, Osoite = "qwerty 123" };
+
+            foreach(Opiskelija op in opiskelijat)
+            {
+                Console.WriteLine("-------------------------------------------");
+                op.PrintData();
+                Console.WriteLine("-------------------------------------------");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Olio/D3T6/D3T6.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3T6
+{
+    class GPU
+    {
+
+        public class Memory
+        {
+            private int MC;
+            private int MS;
+
+            public void PrintData()
+            {
+                Console.WriteLine("    Memory Clock (MHZ): " + MC);
+                Console.WriteLine("    Muistin määrä (GB): " + MS);
+            }
+
+            public Memory(int _MC, int _Memory)
+            {
+
+                MC = _MC;
+                MS = _Memory;
+            }
+
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Nimi: " + Name);
+            Console.WriteLine("Valmistaja: " + Valmistaja);
+            Console.WriteLine("Core Clock (MHZ): " + CC);
+            Console.WriteLine("Muisti: ");
+            Mem.PrintData();
+        }
+
+
+
+        public GPU(string _Name, string _Valmistaja, int _CC, int _MC, int _Memory)
+        {
+            Name = _Name;
+            Valmistaja = _Valmistaja;
+            CC = _CC;
+            Mem = new Memory(_MC, _Memory);
+        }
+
+        private string Name;
+        private string Valmistaja;
+        private int CC;
+        private Memory Mem;
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GPU[] gpus = new GPU[3];
+            gpus[0] = new GPU("Titan X", "Nvidia", 1000, 7000, 12);
+            gpus[1] = new GPU("GTX 750 Ti", "Nvidia", 1020, 5400, 2);
+            gpus[2] = new GPU("R9 390", "AMD", 1060, 6100, 8);
+
+
+
+            foreach (GPU gpu in gpus)
+            {
+                Console.WriteLine("-------------------------------------------");
+                gpu.PrintData();
+                Console.WriteLine("-------------------------------------------");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
