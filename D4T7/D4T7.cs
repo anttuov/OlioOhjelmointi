953d4981dbd4ca93f55883e7f93c4f0c19031e2e
anttuov <k2470@student.jamk.fi>
2016-01-26 19:38:47 +0200

    -

---
+++ D4T7/D4T7.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D4T7
+{
+    class D4T7
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
