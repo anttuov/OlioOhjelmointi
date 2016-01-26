a0f514aec2f0f5b2676ba1eaa5200821183de65e
anttuov <k2470@student.jamk.fi>
2016-01-26 19:44:04 +0200

    -

--- D4T7/D4T7.cs
+++ D4T7/D4T7.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace D4T7
 {
+    class Koira
+    {
+        public string nimi;
+        public int ika;
+        public int sukupuoli;
+    }
+
+    class Rotu : Koira
+    {
+        public string rotu;
+
+    }
     class D4T7
     {
         static void Main(string[] args)
