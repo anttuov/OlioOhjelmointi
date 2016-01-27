8b3c8bfe595a49ac18ead290352a30f813f0d0c7
anttuov <k2470@student.jamk.fi>
2016-01-27 17:15:05 +0200

    -

---
+++ D4T1/D4T1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D4T1
+{
+    class Hissi
+    { 
+    private int floor;
+    public int Floor
+    {
+        get
+        {
+            return floor;
+        }
+
+        set
+        {
+                if (value < 1)
+                    Console.WriteLine("too low");
+                else if (value > 5)
+                    Console.WriteLine("too high");
+                else
+                { 
+                    Console.WriteLine("current floor: "+value);
+                    floor = value;
+                }
+            }
+
+        }
+    }
+    class D4T1
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int number;
+            while (true)
+            {
+                Hissi hissi = new Hissi();
+                Console.Write("floor number > ");
+                line = Console.ReadLine();
+                bool result = int.TryParse(line, out number);
+                // number (integer) was given correctly, use it..
+                if (result)
+                {
+                    hissi.Floor = number;
+                }
+                else
+                    Console.WriteLine("Not a Number");
+            }
+
+        }
+    }
+}
--- D4T7/D4T7.cs
+++ D4T7/D4T7.cs
@@ -10,18 +10,42 @@ namespace D4T7
     {
         public string nimi;
         public int ika;
-        public int sukupuoli;
+        public virtual void PrintData()
+        {
+            Console.WriteLine(nimi + " " + ika);
+        }
     }
 
     class Rotu : Koira
     {
         public string rotu;
+        private readonly int reknumero;
+        public Rotu(int rek)
+        {
+            reknumero = rek;
+        }
+        public override void PrintData()
+        {
+            Console.WriteLine(nimi + " " + ika + " " + rotu + " " + reknumero);
+        }
 
     }
     class D4T7
     {
         static void Main(string[] args)
         {
+            Koira musti = new Koira();
+            musti.nimi = "Musti";
+            musti.ika = 5;
+            musti.PrintData();
+            Rotu fifi = new Rotu(58547217);
+            fifi.nimi = "Fifi";
+            fifi.ika = 1;
+            fifi.rotu = "Chihuahua";
+            fifi.PrintData();
+
+
+
         }
     }
 }
