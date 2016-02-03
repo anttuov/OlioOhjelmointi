73e2679224bb38d84a975b2d8e0003be63f62122
anttuov <k2470@student.jamk.fi>
2016-02-03 18:47:37 +0200

    :^)

---
+++ D6T2/D6T2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6T2
+{
+    class CD
+    {
+        public string name;
+        public string artist;
+        public Dictionary<string, string> songs = new Dictionary<string, string>();
+
+        public void PrintData()
+        {
+            Console.WriteLine("name:" + name);
+            Console.WriteLine("artist:" + artist);
+            Console.WriteLine("songs:");
+            foreach (KeyValuePair<string, string> kvp in songs)
+            {
+                Console.WriteLine("{0} - {1}", kvp.Key, kvp.Value);
+            }
+        }
+
+    }
+    class D6T2
+    {
+        static void Main(string[] args)
+        {
+            CD cd1 = new CD();
+            cd1.artist = "Nightwish";
+            cd1.name = "Endless Forms Most Beautiful";
+            cd1.songs.Add("Shudder Before the Beautiful", "6:29");
+            cd1.songs.Add("Weak Fantasy", "5:23");
+            cd1.songs.Add("Elan", "4:45");
+            cd1.PrintData();
+
+        }
+    }
+}
---
+++ D6T3/D6T3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6T3
+{
+    class D6T3
+    {
+        static void Main(string[] args)
+        {
+            List<string> kortit = new List<string>();
+            string[] Maat = { "Hertta", "Ruutu", "Risti", "Pata" };
+            for(int i=0; i<4; i++)
+            {
+                for(int ii=1; ii<=13; ii++)
+                {
+                    kortit.Add(Maat[i] + ii);
+                }
+            }
+
+            
+            int a = 0;
+            foreach(string s in kortit)
+            {
+                a++;
+                Console.Write(s +", ");
+                if (a % 13 == 0)
+                    Console.Write("\n");
+            }
+            Console.WriteLine("yht " + kortit.Count() + " korttia.");
+
+            //sekoitus
+            var r = new Random();
+            kortit.Sort((x,y) => r.Next(-10,10));
+
+            foreach (string s in kortit)
+            {
+                a++;
+                Console.Write(s + ", ");
+                if (a % 13 == 0)
+                    Console.Write("\n");
+
+            }
+
+        }
+    }
+}
