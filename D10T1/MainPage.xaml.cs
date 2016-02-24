b2c79665c14293416be6fba381cad025dfb0dd5a
anttuov <k2470@student.jamk.fi>
2016-02-24 17:52:48 +0200

    ---

---
+++ D10T1/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace D10T1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void checkBox_Copy_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string lista = "";
+
+            if (Milk.IsChecked == true)
+            {
+                lista = lista + "milk ";
+            }
+            if (Beer.IsChecked == true)
+            {
+                lista = lista + "beer ";
+            }
+            if (Beer2.IsChecked == true)
+            {
+                lista = lista + "beer2 ";
+            }
+            textBox.Text = lista;
+        }
+    }
+}
